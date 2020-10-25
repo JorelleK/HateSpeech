043379da19443890c8022be7e8bc9eb974be5aa4
Jorelle Kebeto <jorellekebeto@gmail.com>
2020-10-24 20:28:55 -0400

    add public folder

---
+++ public/scripts/app.js
@@ -0,0 +1,9 @@
+console.log("APP.js is running!")
+
+// JSX - Javascript XML
+var template = React.createElement("p", null, " This is a JSX test app.js! ");
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
+
+//Babel is javascript compiler
