af107f9ea6610f7ef619a3bb181106f83b332283
JorelleK <32558275+JorelleK@users.noreply.github.com>
2020-10-25 10:46:38 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,56 @@
+// Require our dependencies
+var express = require('express'),
+  exphbs = require('express-handlebars'),
+  http = require('http'),
+  mongoose = require('mongoose'),
+  twitter = require('ntwitter'),
+  routes = require('./routes'),
+  config = require('./webpack.config'),
+  streamHandler = require('./utils/streamHandler');
+
+// Create an express instance and set a port variable
+var app = express();
+var port = process.env.PORT || 8080;
+
+// Set handlebars as the templating engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Disable etag headers on responses
+app.disable('etag');
+
+// Connect to our mongo database
+//mongoose.connect('mongodb+srv://jorelle:manula1990@cluster0.yodjg.mongodb.net/<dbname>?retryWrites=true&w=majority');
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://jorelle:manula1990@cluster0.yodjg.mongodb.net/mydb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+// Create a new ntwitter instance
+var twit = new twitter(config.twitter);
+
+// Index Route
+app.get('/', routes.index);
+
+// Page Route
+app.get('/page/:page/:skip', routes.page);
+
+// Set /public as our static content dir
+app.use("/", express.static(__dirname + "/public/"));
+
+// Fire it up (start our server)
+var server = http.createServer(app).listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
+
+// Initialize socket.io
+var io = require('socket.io').listen(server);
+
+// Set a stream listener for tweets matching tracking keywords
+twit.stream('statuses/filter',{ track: 'banislam, #BanIslam'}, function(stream){
+  streamHandler(stream,io);
+});
